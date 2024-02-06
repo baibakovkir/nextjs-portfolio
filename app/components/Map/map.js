b5dda0583fa436c927d8af6e1ef88586a932ea25
kiryxa09 <baibakovkir@yandex.ru>
2024-02-06 15:50:25 +0300

    feat: added markers

--- app/components/Map/map.js
+++ app/components/Map/map.js
@@ -1,21 +1,45 @@
 'use client';
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import styles from './Map.module.css';
+import { useEffect } from "react";
+
+const points = [
+  { name: "Дом", coordinates: [55.864852, 38.192146] },
+  { name: "Захарово", coordinates: [55.645775, 36.965416] },
+  { name: "Центральный аппарат", coordinates: [55.769276, 37.638884] },
+  // Add more points as needed
+];
 
 const Map = () => {
+  useEffect(() => {
+    // Disable attribution
+    document.getElementsByClassName( 'leaflet-control-attribution' )[0].style.display = 'none';
+  }, []);
 
+  const customIcon = new L.Icon({
+    iconUrl: "https://www.svgrepo.com/show/225946/fields-farm.svg",
+    iconSize: [30, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+  });
+  
   return (
     <MapContainer
       className={styles.map}
       center={[55.8683, 38.1976]}
-      zoom={14}
+      zoom={8}
       scrollWheelZoom={true}
     >
       <TileLayer
-        attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {points.map((point, index) => (
+        <Marker key={index} position={point.coordinates} draggable={false} icon={customIcon}>
+          <Popup>{point.name}</Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
