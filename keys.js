25dfc36a608dc166ce481f88cb816f279b6f9296
kiryxa09 <baibakovkir@yandex.ru>
2024-03-20 17:42:55 +0300

    fix: keys v2

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 const keys = {
   OPENWEATHER_API_KEY: '1b55acadcf8ef2f3ea4f6f89a6d08a90',
-  GITHUB_API_TOKEN: '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'
+  GITHUB_API_TOKEN: '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'
 };
 
 export default keys;
